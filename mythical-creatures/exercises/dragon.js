afb95eed4dc2b67b29555ff19a234ca92513e1df
Ian Ross <ianrossdrums@gmail.com>
2023-04-14 14:42:09 -0600

    Dragon tests pass

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,27 @@
+class Dragon {
+  constructor(name, rider, temperament) {
+    this.name = name;
+    this.rider = rider;
+    this.temperament = temperament;
+    this.timesEaten = 0
+    this.hungry = true;
+  }
 
+  greetRider() {
+    return `Hi, ${this.rider}!`
+  }
 
-module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  eat() {
+    this.timesEaten += 1;
+    if (this.timesEaten >= 3) {
+      this.hungry = false;
+    }
+  }
 }
+
+function findFireBreathers(dragons) {
+  return dragons.filter(function(dragon) {
+    return dragon.temperament === 'aggressive';
+  });
+}
+module.exports = { Dragon, findFireBreathers };
